2ac965dd30e244e445ae989205ade430c9e7d0f4
Rick <rick@landlordwebsolutions.com>
2012-06-17 03:19:36 -0400

    Created first Nodeunit Test, very incomplete

---
+++ test/test-thumbnailer.js
@@ -0,0 +1,31 @@
+var nodeunit = require('nodeunit'),
+    child_process = require('child_process'),
+    thumbnailer = require('../thumbnailer');
+
+exports.as_include = nodeunit.testCase({
+
+    setUp: function(cb) {
+        this._exec = child_process.exec;
+        cb();
+    },
+    tearDown: function(cb) {
+        child_process.exec = this._exec;
+        cb();
+    },
+
+    single_file: function(test) {
+        var self = this;
+        test.expect(1);
+
+        child_process.exec = function(command, callback) {
+            test.equal(command, 'convert "test/images/image.png" -resize 50% "X:\\scripts\\thumbnailer\\test\\test-results\\image.png"');
+            self._exec(command, callback);
+        };
+
+        thumbnailer('test/images/image.png', 'test/test-results/image.png', '50%', function() {
+            test.done();
+        });
+
+    }
+
+});
